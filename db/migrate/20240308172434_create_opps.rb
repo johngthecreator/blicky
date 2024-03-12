5af016cfea40c0b81f5f16ea7bae5653c0adb487
John Gorriceta <johngorriceta@Johns-MacBook-Air.local>
2024-03-12 15:15:45 -0600

    migrations array true

--- db/migrate/20240308172434_create_opps.rb
+++ db/migrate/20240308172434_create_opps.rb
@@ -3,7 +3,7 @@ class CreateOpps < ActiveRecord::Migration[7.1]
     create_table :opps do |t|
       t.string :name
       t.text :opp_reason
-      t.text :progress, array:true
+      t.text :progress, array: true
 
       t.timestamps
     end
